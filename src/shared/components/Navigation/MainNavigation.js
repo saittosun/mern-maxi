a3188344c6e133254eb43b0840095522d5421105
Tosun Sait <tosunsa@cronos.be>
2020-03-25 08:29:37 +0100

    react-transition-group third part library and animating the sidebar are added

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -6,25 +6,36 @@ import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
 import './MainNavigation.css';
 import SideDrawer from './SideDrawer';
+import Backdrop from '../UIElements/Backdrop';
 
 const MainNavigation = props => {
   // manage the state where the drawer is opened or not.
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
-  const openDrawer = () => {
+  const openDrawerHandler = () => {
     setDrawerIsOpen(true);
-  }
+  };
+
+  const closeDrawerHandler = () => {
+    setDrawerIsOpen(false);
+  };
 
   return (
     // React gives you a special wrapper, react.fragment. This doesn't render any real HTML element to the screen, it just fulfills this requirement of having one root element and then in there, you can have as many side by side or nested elements as you want.
     <React.Fragment>
-      {drawerIsOpen && <SideDrawer>
+      {drawerIsOpen && <Backdrop onClick={closeDrawerHandler}/>}
+      {/* {drawerIsOpen && <SideDrawer>
         <nav className="main-navigation__drawer-nav">
           <NavLinks />
         </nav>
-      </SideDrawer>}
+      </SideDrawer>} */}
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
       <MainHeader>
-        <button className="main-navigation__menu-btn" onClick={openDrawer}>
+        <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
           <span />
           <span />
           <span />
--- src/shared/components/Navigation/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer.js
@@ -3,10 +3,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './SideDrawer.css';
+import { CSSTransition } from 'react-transition-group';
 
 const SideDrawer = (props) => {
   // React DOM has more than just a render method. We can also use React DOM.createPortal and now we tell React which content to render where and there I point at content, so what's stored in this content constant and then we need to select a place in the DOM where this content should be rendered when this component is mounted
-  const content = <aside className="side-drawer">{props.children}</aside>
+  // eslint-disable-next-line no-lone-blocks
+  {/* mount on enter and unmount on exit to tell that component and the third-party library that the aside component which is inside of the CSS transition component should really be added to the DOM or be removed from the DOM when it should become visible or invisible, otherwise it's just animated out or in and not really removed, here I want to have it removed or added depending on its state. */}
+  const content = (
+    <CSSTransition 
+      in={props.show} 
+      timeout={200} 
+      classNames="slide-in-left"
+      mountOnEnter
+      unmountOnExit>
+      <aside className="side-drawer" onClick={props.onClick}>{props.children}</aside>   
+    </CSSTransition>
+  )
   return ReactDOM.createPortal(content, document.getElementById('drawer-hook'));
 }
 
